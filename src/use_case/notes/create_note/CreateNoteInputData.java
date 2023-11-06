906528735a47a328fcfe317cd056c65829a4fa9e
TannerBarbecue <danielsawesomed.i.y@gmail.com>
2023-11-06 01:23:08 -0500

    Started implement of CreateNoteController and finished CreateNoteInputData.

--- src/use_case/notes/create_note/CreateNoteInputData.java
+++ src/use_case/notes/create_note/CreateNoteInputData.java
@@ -1,4 +1,28 @@
 package use_case.notes.create_note;
 
+import java.util.ArrayList;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
 public class CreateNoteInputData {
+    final private String title;
+    final private ArrayList<String> tags;
+    final private String author;
+    final private String date;
+
+    public CreateNoteInputData(String title, String tags, String author) {
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        this.title = title;
+        this.author = author;
+        this.date = now.format(format);
+        tags = tags.strip();
+        this.tags = new ArrayList<>(Arrays.asList(tags.split("\\s+")));
+    }
+
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+    public String getDate() { return date; }
+    public ArrayList<String> getTags() { return tags; }
 }
