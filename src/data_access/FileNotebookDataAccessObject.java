cf80b8b3556411c559ebde9644c8b0b1d9c327be
cor1nnama <corinna.ma@mail.utoronto.ca>
2023-11-10 13:05:23 -0500

    specify user and notebook data access

--- src/data_access/FileNotebookDataAccessObject.java
+++ src/data_access/FileNotebookDataAccessObject.java
@@ -1,4 +1,90 @@
 package data_access;
 
-public class FileNotebookDataAccessObject {
+import entity.Notebook;
+import entity.NotebookFactory;
+import entity.User;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class FileNotebookDataAccessObject implements NotebookDataAccessInterface{
+    private final File csvFile;
+
+    private final Map<String, Integer> headers = new LinkedHashMap<>();
+
+    private final Map<String, Notebook> notebookMap = new HashMap<>();
+
+    private NotebookFactory notebookFactory;
+    private UserSignupDataAccessInterface userSignupDataAccessObject;
+
+    public FileNotebookDataAccessObject(String csvPath, NotebookFactory notebookFactory) throws IOException {
+        this.notebookFactory = notebookFactory;
+
+        csvFile = new File(csvPath);
+        headers.put("username", 0);
+        headers.put("notebook", 1);
+
+        if (csvFile.length() == 0) {
+            save();
+        } else {
+
+            try (BufferedReader reader = new BufferedReader(new FileReader(csvFile))) {
+                String header = reader.readLine();
+
+                assert header.equals("username,notebook");
+
+                String row;
+                while ((row = reader.readLine()) != null) {
+                    String[] col = row.split(",");
+                    String username = String.valueOf(col[headers.get("username")]);
+                    String notebookName = String.valueOf(col[headers.get("notebook")]);
+                    if (userSignupDataAccessObject.existsByName(username)){
+                        Map<String, User> accounts = userSignupDataAccessObject.getAccount();
+                        User user = accounts.get(username);
+                        Notebook notebook = notebookFactory.create(notebookName, user);
+                        notebookMap.put(notebookName, notebook);
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void save(Notebook notebook) {
+        notebookMap.put(notebook.getName(), notebook);
+        this.save();
+    }
+
+    private void save() {
+        BufferedWriter writer;
+        try {
+            writer = new BufferedWriter(new FileWriter(csvFile));
+            writer.write(String.join(",", headers.keySet()));
+            writer.newLine();
+
+            for (Notebook notebook : notebookMap.values()) {
+                String line = String.format(notebook.getName(), notebook.getOwner());
+                writer.write(line);
+                writer.newLine();
+            }
+
+            writer.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    /**
+     * Return whether a user exists with username identifier.
+     * @param identifier the username to check.
+     * @return whether a user exists with username identifier
+     */
+    @Override
+    public boolean existsByName(String notebookName) {
+        return notebookMap.containsKey(notebookName);
+    }
 }
--- src/data_access/FileUserDataAccessObject.java
+++ src/data_access/FileUserDataAccessObject.java
@@ -1,4 +1,89 @@
 package data_access;
 
-public class FileUserDataAccessObject {
+import entity.User;
+import entity.UserFactory;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class FileUserDataAccessObject implements UserSignupDataAccessInterface{
+    private final File csvFile;
+
+    private final Map<String, Integer> headers = new LinkedHashMap<>();
+
+    public final Map<String, User> accounts = new HashMap<>();
+
+    private UserFactory userFactory;
+
+    public FileUserDataAccessObject(String csvPath, UserFactory userFactory) throws IOException {
+        this.userFactory = userFactory;
+
+        csvFile = new File(csvPath);
+        headers.put("username", 0);
+        headers.put("password", 1);
+        headers.put("creation_time", 2);
+
+        if (csvFile.length() == 0) {
+            save();
+        } else {
+
+            try (BufferedReader reader = new BufferedReader(new FileReader(csvFile))) {
+                String header = reader.readLine();
+
+                assert header.equals("username,password,creation_time");
+
+                String row;
+                while ((row = reader.readLine()) != null) {
+                    String[] col = row.split(",");
+                    String username = String.valueOf(col[headers.get("username")]);
+                    String password = String.valueOf(col[headers.get("password")]);
+                    User user = userFactory.createUser(username, password);
+                    accounts.put(username, user);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void save(User user) {
+        accounts.put(user.getUsername(), user);
+        this.save();
+    }
+
+    private void save() {
+        BufferedWriter writer;
+        try {
+            writer = new BufferedWriter(new FileWriter(csvFile));
+            writer.write(String.join(",", headers.keySet()));
+            writer.newLine();
+
+            for (User user : accounts.values()) {
+                String line = String.format(
+                        user.getUsername(), user.getPassword());
+                writer.write(line);
+                writer.newLine();
+            }
+
+            writer.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    /**
+     * Return whether a user exists with username identifier.
+     * @param identifier the username to check.
+     * @return whether a user exists with username identifier
+     */
+    @Override
+    public boolean existsByName(String identifier) {
+        return accounts.containsKey(identifier);
+    }
+
+    @Override
+    public Map<String, User> getAccount(){return accounts;}
 }
