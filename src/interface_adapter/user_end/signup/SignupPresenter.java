dc4b46aa3d873acb51a8f122c3c95a454b513dc0
cor1nnama <corinna.ma@mail.utoronto.ca>
2023-11-19 16:54:34 -0500

    login

--- src/interface_adapter/user_end/signup/SignupPresenter.java
+++ src/interface_adapter/user_end/signup/SignupPresenter.java
@@ -1,4 +1,45 @@
 package interface_adapter.user_end.signup;
 
-public class SignupPresenter {
+import interface_adapter.ViewManagerModel;
+import interface_adapter.user_end.login.LoginState;
+import interface_adapter.user_end.login.LoginViewModel;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import use_case.user_end.signup.SignupOutputBoundary;
+import use_case.user_end.signup.SignupOutputData;
+
+public class SignupPresenter implements SignupOutputBoundary {
+
+    private final SignupViewModel signupViewModel;
+    private final LoginViewModel loginViewModel;
+    private ViewManagerModel viewManagerModel;
+
+    public SignupPresenter(ViewManagerModel viewManagerModel,
+                           SignupViewModel signupViewModel,
+                           LoginViewModel loginViewModel) {
+        this.viewManagerModel = viewManagerModel;
+        this.signupViewModel = signupViewModel;
+        this.loginViewModel = loginViewModel;
+    }
+
+    @Override
+    public void prepareSuccessView(SignupOutputData response) {
+        // On success, switch to the login view.
+        SignupState signupState = signupViewModel.getState();
+        LoginState loginState = loginViewModel.getState();
+        loginState.setUsername(signupState.getUsername());
+        this.loginViewModel.setState(loginState);
+        loginViewModel.firePropertyChanged();
+        viewManagerModel.setActiveView(loginViewModel.getViewName());
+        viewManagerModel.firePropertyChanged();
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+        SignupState signupState = signupViewModel.getState();
+        signupState.setUsernameError(error);
+        signupViewModel.firePropertyChanged();
+    }
 }
