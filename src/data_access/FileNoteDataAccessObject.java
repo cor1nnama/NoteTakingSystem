a8f8dd29ceb6b423b3e4a39797642e59bd508672
imtiaz-kidwai <imtiaz.kidwai@mail.utoronto.ca>
2023-11-20 10:36:13 -0500

    Added the methods from the EditNoteDataAccessInterface file

--- src/data_access/FileNoteDataAccessObject.java
+++ src/data_access/FileNoteDataAccessObject.java
@@ -1,6 +1,43 @@
 package data_access;
 
+import entity.Note;
 import use_case.notes.create_note.CreateNoteDataAccessInterface;
 
-public class FileNoteDataAccessObject implements CreateNoteDataAccessInterface {
+import java.util.ArrayList;
+
+public class FileNoteDataAccessObject implements CreateNoteDataAccessInterface, EditNoteDataAccessInterface {
+    @Override
+    public void create(Note note) {
+        //saves note in memory
+    }
+
+    @Override
+    public void save(Note note) {
+
+    }
+
+    @Override
+    public boolean existsByTitle(String title) {
+        return false;
+    }
+
+    @Override
+    public String open(Note note) {
+        return null;
+    }
+
+    @Override
+    public Note getNotebyTitle(String title) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Note> getAllNotes() {
+        return null;
+    }
+
+    @Override
+    public void saveEdit(Note note) {
+
+    }
 }
